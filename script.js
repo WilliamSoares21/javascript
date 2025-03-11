f9bbda1f6030fb7f8f059cf176c6317acebe98ec
William Soares <pedro.williamsoares2022@gmail.com>
2025-03-11 14:39:16 -0300

    Melhorado logica de adição de tarefas com arrays

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
+let tarefas = []
+
+
 function adicionarTarefa() {
-  let inputTarefa = document.getElementById("inputTarefa");
+
+
+
+  const inputTarefa = document.getElementById("inputTarefa");
   let tarefa = inputTarefa.value.trim()//trim polda para que o espaçamento no começo e final sejam ajustados
+  const mensagem = document.getElementById("mensagem")
+
 
   if (tarefa == "") {
     let mensagemVazia = "Área vazia, tente adicionar uma tarefa";
-    document.getElementById("mensagem").textContent = mensagemVazia;
+    mensagem.textContent = mensagemVazia;
   } else {
     let mensagemSucesso = "Tarefa adicionada com sucesso!";
-    document.getElementById("mensagem").textContent = mensagemSucesso;
+    mensagem.textContent = mensagemSucesso;
 
-    let listaTarefas = document.getElementById("listaTarefas")
-
-    let novaTarefa = document.createElement("li")
-    novaTarefa.textContent = tarefa
-
-    listaTarefas.appendChild(novaTarefa)
+    tarefas.push(tarefa)
+    renderizarTarefas()
   }
   //limpa o inpput do usuário
   inputTarefa.value = ""
+
+}
+
+function renderizarTarefas() {
+  const listaTarefas = document.getElementById("listaTarefas")
+
+  listaTarefas.innerHTML = ""
+  let i = 0;
+  for (i; i < tarefas.length; i++) {
+    let novaTarefa = document.createElement("li")
+    novaTarefa.textContent = tarefas[i]
+    listaTarefas.appendChild(novaTarefa)
+  }
+
+
+
 
 }
