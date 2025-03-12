5f62455a003ddf5dcd69c23efd1c3c280bee124c
William Soares <pedro.williamsoares2022@gmail.com>
2025-03-12 12:57:43 -0300

    Adicionado logica de botão editar, remover e para limpar lista de tarefas

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ let tarefas = []
 
 function adicionarTarefa() {
 
-
-
   const inputTarefa = document.getElementById("inputTarefa");
   let tarefa = inputTarefa.value.trim()//trim polda para que o espaçamento no começo e final sejam ajustados
+
   const mensagem = document.getElementById("mensagem")
 
 
@@ -29,14 +28,49 @@ function renderizarTarefas() {
   const listaTarefas = document.getElementById("listaTarefas")
 
   listaTarefas.innerHTML = ""
-  let i = 0;
-  for (i; i < tarefas.length; i++) {
+
+  for (let i = 0; i < tarefas.length; i++) {
     let novaTarefa = document.createElement("li")
     novaTarefa.textContent = tarefas[i]
+
+    let botaoRemover = document.createElement("button")
+    botaoRemover.className = "remover"
+    botaoRemover.textContent = "Remover"
+    botaoRemover.onclick = () => removerTarefa(i)
+
+    let botaoEditar = document.createElement("button")
+    botaoEditar.className = "editar"
+    botaoEditar.textContent = "Editar"
+    botaoEditar.onclick = () => editarTarefa(i)
+
+
+
+    novaTarefa.appendChild(botaoRemover)
+    novaTarefa.appendChild(botaoEditar)
     listaTarefas.appendChild(novaTarefa)
   }
 
+}
+
+function removerTarefa(i) {
+
+  tarefas.splice(i, 1)
+  renderizarTarefas()
+}
 
 
+function editarTarefa(i) {
+  let tarefaEditada = prompt("Edite a tarefa: ")
+  if (tarefaEditada.trim() !== "") {
+    tarefas[i] = tarefaEditada
+    renderizarTarefas()
 
+  }
+}
+
+function limparLista() {
+  tarefas.length = 0;
+  renderizarTarefas()
+  const mensagem = document.getElementById("mensagem")
+  mensagem.textContent = "Lista limpa com sucesso !"
 }
